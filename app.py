381fd110bb210db6ecb6e33818f2d93623d596c5
Kajana2005 <89174934+Kajana2005@users.noreply.github.com>
2021-09-30 10:00:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'nffSwLOWJs2fCY82dxX9nZJ7NLS0qCTen1W4RfUOjaiLWWqxF37lwtTdpEZ8ReVShfKYzyirdkZGhiJWOZW5RN+IO9CCB1NnYYglkSrCUdo5tn7yUoG+PJvxOFV4vW/5nNabJHDoTIsqBy1Zc7E9OgdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
